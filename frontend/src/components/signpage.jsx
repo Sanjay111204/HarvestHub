6b23fa70f55cf5ee28bcf40c6faaf9f71ce7dd11
Sanjay111204 <polakalasanjay@gmail.com>
2025-06-16 20:44:19 +0530

    Update files with latest changes

--- frontend/src/components/signpage.jsx
+++ frontend/src/components/signpage.jsx
@@ -51,10 +51,13 @@ const signpage = () => {
   };
   const Loginfree = async () => {
     try {
-      const res = await axios.post("http://localhost:1112/api/auth/login", {
-        email: "User111204",
-        password: "111204",
-      });
+      const res = await axios.post(
+        "https://harvesthub-h4eh.onrender.com/api/auth/login",
+        {
+          email: "User111204",
+          password: "111204",
+        }
+      );
       alert("Login successful!");
       // You can store the token or redirect from here if needed
       console.log(res.data.token);
